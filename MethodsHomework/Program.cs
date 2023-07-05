4bfc0bc94c0023038fc1ff27dfbee2a821e9deb0
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:18:03 +0200

    add app

---
+++ MethodsHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using MethodsHomework;
+using static MethodsHomework.UserMethods;
+
+string userName;
+bool isInvalid;
+
+do
+{
+    userName = UserMethods.GetUserName();
+    isInvalid = int.TryParse(userName, out int dumb);
+    if (isInvalid || string.IsNullOrEmpty(userName))
+    {
+        Console.WriteLine("Please provide a valid name");
+    }
+    if (userName != "exit" && !isInvalid && !string.IsNullOrEmpty(userName))
+    {
+        UserMethods.SayHello(userName);
+    }
+} while (userName != "exit");
---
+++ MethodsHomework/UserMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsHomework
+{
+    public static class UserMethods
+    {
+        public static string GetUserName()
+        {
+            Console.Write("What is your name (or exit): ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        public static void SayHello(string userName)
+        {
+            Console.WriteLine($"your name is {userName}");
+        }
+    }
+}
